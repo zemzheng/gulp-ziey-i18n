ead088e44ed4a4bce78fc628aadec197f81925d1
Zem Zheng <zemzheng@gmail.com>
2015-07-14 14:44:12 +0800

    a test - case Update #2

--- test/index.js
+++ test/index.js
@@ -26,7 +26,12 @@ describe( 'gulp - stream', function(){
             lang : 'en',
             po   : gettext.obj2po({
                 a : { str : "A" },
-                b : { str : 'B' }
+                b : { str : 'B' },
+
+                // #2 - case 
+                "测试更多" : { str : "Test \"More\"" },
+                "这是[更多]按钮" : { str : "It's the Button of \"More\"" }
+                
             })
         },
         stream = i18n( opt );
@@ -52,6 +57,17 @@ describe( 'gulp - stream', function(){
         '{{= _("音乐及榜单管理 | %s") }}', '音乐及榜单管理 | %s'
     );
 
+    check( '输出文本中单双引号带转义 - base', stream,
+        '{{= _("\'") }}' + "{{= _('\"') }}", "\\'" + '\\"'
+    );
+
+    check( '输出文本中单双引号带转义 - case', stream,
+        "{{= _( '测试更多' ) }}" + '{{= _( \'这是[更多]按钮\' ) }}',
+        'Test \\"More\\"' + 'It\\\'s the Button of \\"More\\"'
+    );
+
+    
+
     stream.once( 'data', function( outputFile ){
         it( 'xgettext', function(){
             var input = gettext.po2obj( outputFile.contents.toString() ),
@@ -60,7 +76,11 @@ describe( 'gulp - stream', function(){
                     c : "",
                     d : "",
                     b : 'B',
-                    "音乐及榜单管理 | %s" : ""
+                    "音乐及榜单管理 | %s" : "",
+                    '"' : '',
+                    "'" : '',
+                    "测试更多"       : "Test \"More\"",
+                    "这是[更多]按钮" : "It's the Button of \"More\"",
                 },
                 key;
             for( key in input ){
